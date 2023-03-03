976ff7bf548835667f82ede2c3a649913e124d58
tkalra11 <tkalra1111@gmail.com>
2023-03-03 15:10:12 +0530

    Selection sort

--- practice/BubbleSort.java
+++ practice/BubbleSort.java
@@ -1,2 +1,25 @@
-package practice;public class BubbleSort {
+package practice;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = { 1 , 4 , 5 , -1 , -1 };
+        sort( arr );
+        System.out.println(Arrays.toString( arr ));
+    }
+    static void sort(int[] arr){
+        for ( int i = 0 ; i < arr.length ; i++ ) {
+            boolean swapped=false;
+            for (int j = 1; j < arr.length - i ; j++ ) {
+                if( arr[j-1] > arr[j]){
+                    int temp = arr[j] ;
+                    arr[j] = arr[j-1] ;
+                    arr[j-1] = temp ;
+                    swapped = true ;
+                }
+            }
+            if(!swapped) break ;
+        }
+    }
 }
---
+++ practice/SelectionSort.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = { 1 , 4 , -1 , 5 , 3 };
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort( int[] arr ){
+        for ( int i = 0 ; i <= arr.length -1 ; i++ ) {
+            int last = arr.length - 1 - i ;
+
+            int maxElem=arr[0];
+            for (int j = 1; j <= last; j++) {
+                if( arr[j] > arr[maxElem] )
+                    maxElem = j ;
+            }
+
+            int temp = arr[last] ;
+            arr[last] = arr[maxElem] ;
+            arr[maxElem] = temp ;
+        }
+    }
+}
